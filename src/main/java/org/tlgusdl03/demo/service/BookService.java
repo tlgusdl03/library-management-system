ae1dd93f26f03a3a07e21ce73c8311d04412f375
tlgusdl03 <tlgusdl03@gmail.com>
2026-05-25 18:54:44 +0900

    feat : 데이터베이스 더미 데이터 삽입 파일 추가

--- src/main/java/org/tlgusdl03/demo/service/BookService.java
+++ src/main/java/org/tlgusdl03/demo/service/BookService.java
@@ -1,9 +1,12 @@
 package org.tlgusdl03.demo.service;
 
 import org.tlgusdl03.demo.dto.BookRegisterRequest;
+import org.tlgusdl03.demo.entities.Books;
 
 public interface BookService {
     Long registerBook(BookRegisterRequest request);
-
-
+    Books searchById(Long bookId);
+    Books searchByIsbn(String isbn);
+    Books searchByTitle(String title);
+    Books searchByAuthor(String author);
 }
