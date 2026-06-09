b4498e67a25fbe4d706b893935783b37586c773d
tlgusdl03 <tlgusdl03@gmail.com>
2026-06-09 23:34:19 +0900

    feat : bookService 테스트 코드 추가

--- src/test/java/org/tlgusdl03/demo/service/book/BookServiceTest.java
+++ src/test/java/org/tlgusdl03/demo/service/book/BookServiceTest.java
@@ -11,7 +11,10 @@ import org.tlgusdl03.demo.repository.BookCopiesRepository;
 import org.tlgusdl03.demo.repository.BooksRepository;
 import org.tlgusdl03.demo.service.BookService;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 @SpringBootTest
 @Transactional
@@ -39,5 +42,78 @@ public class BookServiceTest {
         assertThat(bookResponse.getIsbn()).isEqualTo("test01");
         assertThat(bookResponse.getTitle()).isEqualTo("제목1");
         assertThat(bookResponse.getAuthor()).isEqualTo("작가1");
+
+    }
+
+    @Test
+    @DisplayName("Isbn을 통한 책 검색 기능을 테스트 합니다.")
+    void searchBookByIsbnTest() {
+        BookRegisterRequest bookRegisterRequest = new BookRegisterRequest();
+        
+        bookRegisterRequest.setIsbn("test01");
+        bookRegisterRequest.setTitle("제목1");
+        bookRegisterRequest.setAuthor("작가1");
+
+        Long savedId =  bookService.registerBook(bookRegisterRequest);
+        BookResponse bookResponse1 = bookService.searchById(savedId);
+        BookResponse bookResponse2 = bookService.searchByIsbn("test01");
+
+        assertThat(bookResponse1.getIsbn()).isEqualTo("test01");
+        assertThat(bookResponse2.getIsbn()).isEqualTo("test01");
+        assertThat(bookResponse1.getTitle()).isEqualTo("제목1");
+        assertThat(bookResponse2.getTitle()).isEqualTo("제목1");
+        assertThat(bookResponse1.getAuthor()).isEqualTo("작가1");
+        assertThat(bookResponse2.getAuthor()).isEqualTo("작가1");
+
+    }
+
+    @Test
+    @DisplayName("Title을 통한 책 검색 기능을 테스트 합니다.")
+    void searchBookByTitleTest() {
+        BookRegisterRequest bookRegisterRequest = new BookRegisterRequest();
+
+        bookRegisterRequest.setIsbn("test01");
+        bookRegisterRequest.setTitle("제목1");
+        bookRegisterRequest.setAuthor("작가1");
+
+        Long savedId =  bookService.registerBook(bookRegisterRequest);
+        BookResponse bookResponse1 = bookService.searchById(savedId);
+        BookResponse bookResponse2 = bookService.searchByTitle("제목1");
+
+        assertThat(bookResponse1.getIsbn()).isEqualTo("test01");
+        assertThat(bookResponse2.getIsbn()).isEqualTo("test01");
+        assertThat(bookResponse1.getTitle()).isEqualTo("제목1");
+        assertThat(bookResponse2.getTitle()).isEqualTo("제목1");
+        assertThat(bookResponse1.getAuthor()).isEqualTo("작가1");
+        assertThat(bookResponse2.getAuthor()).isEqualTo("작가1");
+
+    }
+
+    @Test
+    @DisplayName("Author를 통한 책 검색 기능을 테스트 합니다.")
+    void searchBookByAuthorTest() {
+        BookRegisterRequest bookRegisterRequest = new BookRegisterRequest();
+        bookRegisterRequest.setIsbn("test01");
+        bookRegisterRequest.setTitle("제목1");
+        bookRegisterRequest.setAuthor("작가1");
+        bookService.registerBook(bookRegisterRequest);
+
+        BookRegisterRequest bookRegisterRequest2 = new BookRegisterRequest();
+        bookRegisterRequest2.setIsbn("test02");
+        bookRegisterRequest2.setTitle("제목2");
+        bookRegisterRequest2.setAuthor("작가1");
+        bookService.registerBook(bookRegisterRequest2);
+
+        List<BookResponse> bookResponseList = bookService.searchByAuthor("작가1");
+
+        assertThat(bookResponseList).hasSize(2);
+
+        assertThat(bookResponseList)
+                .extracting(BookResponse::getIsbn, BookResponse::getTitle, BookResponse::getAuthor)
+                .containsExactly(
+                        tuple("test01","제목1","작가1"),
+                        tuple("test02","제목2","작가1")
+                );
+
     }
 }
